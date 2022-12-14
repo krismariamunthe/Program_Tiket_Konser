b4f20e61d59b0f654c0ce614db1cfbc11bf8e96a
paraboyy <114977929+krismariamunthe@users.noreply.github.com>
2022-12-14 22:10:59 +0800

    membuat menu info program

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include "proses.c"
 
-
+void info();
 
 int main(){
     home_page();
@@ -20,6 +20,7 @@ int home_page() {
     printf("\t\t\t|------------------------------------------------|\n");
     printf("\t\t\t|  [1]  PEMBELIAN TIKET                          |\n");
     printf("\t\t\t|  [2]  LIST TIKET                               |\n");
+    printf("\t\t\t|  [3]  INFO PROGRAM                             |\n");
     printf("\t\t\t|------------------------------------------------|\n");
     printf("\t\t\t>> ");
     pilihan = inputan();
@@ -35,6 +36,9 @@ int home_page() {
             system("cls");
             list_tiket();
         break;
+        case 3:
+        	info();
+        break;
         default :
             system("cls");
             printf("\t\t\t---------------------------------------------\n");
@@ -46,6 +50,27 @@ int home_page() {
         break;
     }
 return 0;
+}
+
+void info(){
+	system("cls");
+	printf("\t\t\t--------------------------------------------------\n");
+    printf("\t\t\t|<<<<<<<<<<<<<<   INFO PROGRAM   >>>>>>>>>>>>>>>>|\n");
+    printf("\t\t\t|------------------------------------------------|\n");
+    printf("\t\t\t|         PROGRAM PEMBELIAN TIKET KONSER         |\n");
+    printf("\t\t\t|          TUBES ALGORITMA PEMROGRAMAN           |\n");
+    printf("\t\t\t|           UNIVERSITAS UDAYANA 2022             |\n");
+    printf("\t\t\t|------------------------------------------------|\n");
+    printf("\t\t\t|            PROGRAM INI DIBUAT OLEH             |\n");
+    printf("\t\t\t|------------------------------------------------|\n");
+    printf("\t\t\t|        NAMA             |         NIM          |\n");
+    printf("\t\t\t|------------------------------------------------|\n");
+    printf("\t\t\t|  [-]  KRISMARIA MUNTHE  |     2205551045       |\n");
+    printf("\t\t\t|  [-]                    |     2205551000       |\n");
+    printf("\t\t\t|------------------------------------------------|\n");
+    system("pause");
+    system("cls");
+    home_page();
 }
 
 int inputan(){
