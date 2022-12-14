024e7f1878021fbdc6498e9bb6a0c47d7c821b7d
paraboyy <114977929+krismariamunthe@users.noreply.github.com>
2022-12-15 00:02:08 +0800

    mengubah program
    Jadi disini saya mengubah alur program dikarenakan seblumnya masih ada banyak kesalahan pada cara kerja program

--- cetak.c
+++ cetak.c
@@ -5,33 +5,45 @@
 int x;
 time_t waktu;
 
-void struk_tiket(){
+void cetak_tiket(int n, char nama[30],char email[15], char tlpn[15], char phari[10], char pclas[10]);
+
+void struk_tiket(int n, char nama[30], char email[15], char tlpn[15], char phari[10], char pclas[10]){
 	
-	x = 200+rand()%900;
+    int i;
+
+
 	system("cls");
-	printf("\t\t\t--------------------------------------------------\n");
-    printf("\t\t\t|<<<<<<<<<<<<<<<<   STRUK TIKET   >>>>>>>>>>>>>>>|\n");
-    printf("\t\t\t|------------------------------------------------|\n");
-    printf("\t\t\t|                                                |\n");
-    printf("\t\t\t|  NOMOR TIKET    >  %d\t\t\t|\n", x);
-    printf("\t\t\t|                                                |\n");
-	printf("\t\t\t|------------------------------------------------|\n");   
-    printf("\t\t\t|  NOTE!!! UNTUK JAM KONSER DIMULAI PUKUL 18.00  |\n");
-    printf("\t\t\t|------------------------------------------------|\n");
+    for(i = 1; i<=n;i++){
+        x = 200+rand()%900;
+        printf("\t\t\t--------------------------------------------------\n");
+        printf("\t\t\t|<<<<<<<<<<<<<<<<   STRUK TIKET   >>>>>>>>>>>>>>>|\n");
+        printf("\t\t\t|------------------------------------------------|\n");
+        printf("\t\t\t|                                                |\n");
+        printf("\t\t\t|  NOMOR TIKET    >  %d\t\t\t |\n", x);
+        printf("\t\t\t|  CLASS TIKET    >  %s\t\t\t |\n", pclas);
+        printf("\t\t\t|  DAY            >  %s\t\t\t |\n", phari);
+        printf("\t\t\t|                                                |\n");
+        printf("\t\t\t|------------------------------------------------|\n");   
+        printf("\t\t\t|  NOTE!!! UNTUK JAM KONSER DIMULAI PUKUL 18.00  |\n");
+        printf("\t\t\t|------------------------------------------------|\n\n\n");
+        cetak_tiket(n, nama, email, tlpn, phari, pclas);
+    }
 }
 
-void cetak_tiket(char nama[30],char email[15], char tlpn[15]){
-	FILE* tiket=fopen("struk tiket.txt", "w+");
+void cetak_tiket(int n, char nama[30],char email[15], char tlpn[15], char phari[10], char pclas[10]){
+	FILE* tiket=fopen("struk tiket.txt", "a+");
 	
 	time(&waktu);
+
 	fprintf(tiket,"\t\t\t--------------------------------------------------\n");
     fprintf(tiket,"\t\t\t|<<<<<<<<<<<<<<<<   STRUK TIKET   >>>>>>>>>>>>>>>|\n");
     fprintf(tiket,"\t\t\t|------------------------------------------------|\n");
-    fprintf(tiket,"\t\t\t|  NOMOR TIKET    >  %d\t\t\t\t|\n", x);
-    fprintf(tiket,"\t\t\t|  NAMA PELANGGAN >  %s\t\t\t\t|\n", nama);
-    fprintf(tiket,"\t\t\t|  NOMOR TELEPON  >  %s\t\t\t\t|\n", tlpn);
-    fprintf(tiket,"\t\t\t|  EMAIL          >  %s\t\t\t\t|\n", email);
-    fprintf(tiket,"\t\t\t|  EMAIL          >  %s\t\t|\n", ctime (&waktu));
-	fprintf(tiket,"\t\t\t|------------------------------------------------|\n");   
-    fprintf(tiket,"\t\t\t>> ");
+    fprintf(tiket,"\t\t\t|  NOMOR TIKET    >  %d\t\t\t\t\t\t|\n", x);
+    fprintf(tiket,"\t\t\t|  CLASS TIKET    >  %s\t\t\t\t\t\t|\n", pclas);
+    fprintf(tiket,"\t\t\t|  DAY            >  %s\t\t\t\t\t\t|\n", phari);
+    fprintf(tiket,"\t\t\t|  NAMA PELANGGAN >  %s\t\t\t\t\t\t|\n", nama);
+    fprintf(tiket,"\t\t\t|  NOMOR TELEPON  >  %s\t\t\t\t\t\t|\n", tlpn);
+    fprintf(tiket,"\t\t\t|  EMAIL          >  %s\t\t\t\t\t\t|\n", email);
+    fprintf(tiket,"\t\t\t|  DATE PEMBELIAN >  %s\t\t\t\t|\n", ctime (&waktu));
+	fprintf(tiket,"\t\t\t|------------------------------------------------|\n\n");   
 }
--- proses.c
+++ proses.c
@@ -3,15 +3,17 @@
 #include <string.h>
 #include "cetak.c"
 
-char phari[10];
-char nama[30], email[15], tlpn[15];
-int tiket = 0, clas = 0;
+char phari[10]; //MENAMPUNG DATA HARI PEMBELIAN TIKET
+char pclas[10]; //MENAMPUNG DATA CLASS TIKET
+char nama[30], email[15], tlpn[15]; //MENAMPUNG DATA DIRI PEMBELI TIKET
+int n; //BANYAK TIKET YANG DI BELI
 
 
 void hari();
 void pembelian();
 void clas_tiket();
 void pilih_hari();
+void pilih_tiket();
 
 void list_tiket(){
     int jenis;
@@ -35,7 +37,6 @@ kembali:
         break;
         case 2:
             system("cls");
-            tiket = 1;
             clas_tiket();
         break;
         case 0:
@@ -107,6 +108,7 @@ kembali:
 
 void pembelian(){
 
+    system("cls");
     printf("\t\t\t--------------------------------------------------\n");
     printf("\t\t\t|             MASUKAN DATA DIRI ANDA             |\n");
     printf("\t\t\t--------------------------------------------------\n");
@@ -118,16 +120,14 @@ void pembelian(){
     scanf("%s", &email);
     printf("\t\t\tNo HP\n\t\t\t>> ");
     scanf("%s", &tlpn);
+    printf("\t\t\tJumlah tiket dibeli\n\t\t\t>> ");
+    scanf("%d", &n);
     
-    if(clas > 0){
-    	system("cls");
-    	struk_tiket();
-    	cetak_tiket(nama, email, tlpn);
-	}else{
-		system("cls");
-		clas_tiket();
-	}
-
+    system("cls");
+    pilih_tiket();
+    system("cls");
+    pilih_hari();
+    // system("cls");
 }
 
 
@@ -140,49 +140,39 @@ kembali:
     printf("\t\t\t|------------------------------------------------|\n");
     printf("\t\t\t|     TIKET     |    HARGA    |       MENU       |\n");
     printf("\t\t\t|------------------------------------------------|\n");
-    printf("\t\t\t|  [1]  VVIP    | Rp.500.000  |  [9]  HOME       |\n");
-    printf("\t\t\t|  [2]  GOLD    | Rp.300.000  |  [0]  BACK       |\n");
-    printf("\t\t\t|  [3]  SILVER  | Rp.150.000  |                  |\n");
+    printf("\t\t\t|  [-]  VVIP    | Rp.500.000  |  [9]  HOME       |\n");
+    printf("\t\t\t|  [-]  GOLD    | Rp.300.000  |  [0]  BACK       |\n");
+    printf("\t\t\t|  [-]  SILVER  | Rp.150.000  |                  |\n");
+    printf("\t\t\t|------------------------------------------------|\n");
+	printf("\t\t\t|  BELI TIKET   [1] YA   |||     [2]  TIDAK      |\n");
     printf("\t\t\t|------------------------------------------------|\n");   
     printf("\t\t\t>> ");
     scanf("%d", &jenis);
     fflush(stdin);
 
-	if(tiket > 0){
-	    switch (jenis)
-	    {
-	    case 1:
-	    case 2:
-	    case 3:
-	        system("cls");
-	        pilih_hari();
-	        system("cls");
-	        clas = 1;
-	        pembelian();
-	    break;
-	    case 9:
-	        system("cls");
-	        home_page();
-	    break;
-	    case 0:
-	        system("cls");
-	        list_tiket();
-	    break;
-	    default :
-	        system("cls");
-	        printf("\t\t\t---------------------------------------------\n");
-	        printf("\t\t\t|               INPUTAN ERROR               |\n");
-	        printf("\t\t\t---------------------------------------------\n");
-	        system("pause");
-	        system("cls");
-	        goto kembali;
-	    break;
-    	}
-	}else{
-		system("cls");
-		pilih_hari();
-		struk_tiket();
-	}
+	switch(jenis){
+	case 1:
+		pembelian();
+	break;
+	case 2:
+		exit(0);
+	break;
+	case 0:
+		list_tiket();
+	break;
+	case 9:
+		home_page();
+	break;
+    default :
+        system("cls");
+        printf("\t\t\t---------------------------------------------\n");
+        printf("\t\t\t|               INPUTAN ERROR               |\n");
+        printf("\t\t\t---------------------------------------------\n");
+        system("pause");
+        system("cls");
+        goto kembali;
+    break;
+   	}
 }
 
 
@@ -192,11 +182,11 @@ kembali:
     printf("\t\t\t--------------------------------------------------\n");
     printf("\t\t\t|<<<<<<<<<<<<<<<   JADWAL TIKET   >>>>>>>>>>>>>>>|\n");
     printf("\t\t\t|------------------------------------------------|\n");
-    printf("\t\t\t|           HARI          |        MENU          |\n");
+    printf("\t\t\t|                   PILIH HARI                   |\n");
     printf("\t\t\t|------------------------------------------------|\n");
-    printf("\t\t\t|  [1]  JUMAT             |  [9]  HOME           |\n");
-    printf("\t\t\t|  [2]  SABTU             |  [0]  BACK           |\n");
-    printf("\t\t\t|  [3]  MINGGU            |                      |\n");
+    printf("\t\t\t|  [1]  JUMAT                                    |\n");
+    printf("\t\t\t|  [2]  SABTU                                    |\n");
+    printf("\t\t\t|  [3]  MINGGU                                   |\n");
     printf("\t\t\t|------------------------------------------------|\n");
     printf("\t\t\t|  NOTE!!! UNTUK JAM KONSER DIMULAI PUKUL 18.00  |\n");
     printf("\t\t\t|------------------------------------------------|\n");
@@ -207,18 +197,15 @@ kembali:
     switch (hari){
     	case 1:
     		strcpy(phari, "Jum'at");
+            struk_tiket(n, nama, email, tlpn, phari, pclas);
     	break;
     	case 2:
     		strcpy(phari, "Sabtu");
+            struk_tiket(n, nama, email, tlpn, phari, pclas);
     	break;
     	case 3:
     		strcpy(phari, "Minggu");
-    	break;
-    	case 0:
-    		clas_tiket();
-    	break;
-    	case 9:
-    		home_page();
+            struk_tiket(n, nama, email, tlpn, phari, pclas);
     	break;
     	default:
     		system("cls");
@@ -229,5 +216,46 @@ kembali:
 	        system("cls");
 	        goto kembali;
     	break;
+    }
+}
+
+void pilih_tiket(){
+    int jenis;
+
+kembali:
+    printf("\t\t\t--------------------------------------------------\n");
+    printf("\t\t\t|<<<<<<<<<<<<<<<<   HARGA TIKET   >>>>>>>>>>>>>>>|\n");
+    printf("\t\t\t|------------------------------------------------|\n");
+    printf("\t\t\t|     TIKET              |    HARGA              |\n");
+    printf("\t\t\t|------------------------------------------------|\n");
+    printf("\t\t\t|  [1]  VVIP             |   Rp.500.000          |\n");
+    printf("\t\t\t|  [2]  GOLD             |   Rp.300.000          |\n");
+    printf("\t\t\t|  [3]  SILVER           |   Rp.150.000          |\n");
+    printf("\t\t\t|------------------------------------------------|\n");  
+    printf("\t\t\t>> ");
+    scanf("%d", &jenis);
+    fflush(stdin);
+
+    switch (jenis)
+    {
+    case 1:
+        strcpy(pclas, "VVIP");
+    break;
+    case 2:
+        strcpy(pclas, "GOLD");
+    break;
+    case 3:
+        strcpy(pclas, "SILVER");
+    break;
+    break;
+    default :
+        system("cls");
+        printf("\t\t\t---------------------------------------------\n");
+        printf("\t\t\t|               INPUTAN ERROR               |\n");
+        printf("\t\t\t---------------------------------------------\n");
+        system("pause");
+        system("cls");
+        goto kembali;
+    break;
     }
 }
